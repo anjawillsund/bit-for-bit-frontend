70a6ab9281892de54dc7a94e37308c5df51396b6
Anja Willsund <aw22hs@student.lnu.se>
2024-04-16 17:55:13 +0200

    Add fetchUserPuzzles() and several variables

--- src/components/contexts/PuzzleContext.js
+++ src/components/contexts/PuzzleContext.js
@@ -1,5 +1,5 @@
 import { createContext, useState } from 'react'
-// import { useTokenContext } from './TokenContext'
+import { useTokenContext } from './TokenContext'
 
 /**
  * Context for managing movie-related data and functions.
@@ -14,22 +14,53 @@ export const PuzzleContext = createContext()
  * @returns {JSX.Element} The JSX element representing the PuzzleContextProvider component.
  */
 const PuzzleContextProvider = ({ children }) => {
-  // const fetchWithToken = useTokenContext()
+  const fetchWithToken = useTokenContext()
 
   const [isUserActive, setIsUserActive] = useState(false)
+  const [puzzles, setPuzzles] = useState([])
+  const [puzzlesArray, setPuzzlesArray] = useState([])
+  const [isLoadingPuzzles, setIsLoadingPuzzles] = useState(true)
+
+  const fetchUserPuzzles = async () => {
+    try {
+      console.log('Fetching user puzzles')
+      const response = await fetchWithToken('http://localhost:8090/my/puzzles', {
+        method: 'GET'
+      })
+      if (response.ok) {
+        const puzzles = await response.json()
+        console.log(puzzles)
+        setPuzzlesArray(puzzles)
+      } else {
+        console.log('Could not fetch user puzzles')
+      }
+    } catch (error) {
+      console.log(error)
+    }
+  }
 
   /**
    * Resets the state of the PuzzleContextProvider component.
    */
   const resetState = () => {
+    setPuzzles([])
+    setPuzzlesArray([])
+    setIsLoadingPuzzles(true)
     setIsUserActive(false)
   }
 
   return <PuzzleContext.Provider
     value={{
+      fetchUserPuzzles,
+      isLoadingPuzzles,
       isUserActive,
+      puzzles,
+      puzzlesArray,
+      resetState,
+      setIsLoadingPuzzles,
       setIsUserActive,
-      resetState
+      setPuzzles,
+      setPuzzlesArray
     }}>
     {children}
   </PuzzleContext.Provider>
