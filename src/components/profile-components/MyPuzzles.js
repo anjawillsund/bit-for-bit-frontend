16be129cab7fd58932938da76709cbf3243e61d1
Anja Willsund <aw22hs@student.lnu.se>
2024-04-17 10:35:35 +0200

    Add title and pieces to display info

--- src/components/profile-components/MyPuzzles.js
+++ src/components/profile-components/MyPuzzles.js
@@ -1,4 +1,5 @@
 import Button from '../Button'
+import React from 'react'
 import { useContext, useEffect } from 'react'
 import { PuzzleContext } from '../contexts/PuzzleContext'
 import { useNavigate } from 'react-router-dom'
@@ -18,15 +19,14 @@ const MyPuzzles = () => {
 
   useEffect(() => {
     async function loadData() {
-		await fetchUserPuzzles()
-    setIsLoadingPuzzles(false)
-	  }
+      await fetchUserPuzzles()
+      setIsLoadingPuzzles(false)
+    }
     loadData()
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [])
 
   const handleLogout = async () => {
-    resetState()
     try {
       const response = await fetchWithToken('http://localhost:8090/logout', {
         method: 'GET'
@@ -45,31 +45,33 @@ const MyPuzzles = () => {
   }
 
   return (
-    <div className='self-matches matches'>
+    <div className='puzzles'>
       <Button
-				onClick={handleLogout}
-				buttonText='Logout'
-			/>
-			{isLoadingPuzzles ? (
-				<p>Loading...</p>
-			) : (
-				<ul>
-					{puzzlesArray.length > 0 ? (puzzlesArray.map(puzzle => (
-						<li key={puzzle._id}>
-							{Object.entries(puzzle).map(([key, value], index) => (
-                key === 'imageUrl' && (
-									<img key={`${puzzle.puzzleId}`} src={value} alt={puzzle.title} />
-								)
-							))}
-						</li>
-					))
-					) : (
-						<p>No puzzles to display</p>
-					)}
-				</ul>
-			)}
-		</div>
-	)
+        onClick={handleLogout}
+        buttonText='Logout'
+      />
+      {isLoadingPuzzles ? (
+        <p>Loading...</p>
+      ) : (
+        <ul>
+          {puzzlesArray.length > 0 ? (puzzlesArray.map(puzzle => (
+            <li key={puzzle._id} className="puzzle-item">
+              <a href={`/puzzle/${puzzle._id}`}>
+              <img src={puzzle.imageUrl} alt={''} />
+              <div className="text-content">
+                <h3>{puzzle.title}</h3>
+                <p>Pieces: {puzzle.piecesNumber}</p>
+              </div>
+              </a>
+            </li>
+          ))
+          ) : (
+            <p>No puzzles to display</p>
+          )}
+        </ul>
+      )}
+    </div>
+  )
 }
 
 export default MyPuzzles
