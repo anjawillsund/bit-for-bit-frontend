0ae14e793603008781c255aa7f0710c312810039
Anja Willsund <aw22hs@student.lnu.se>
2024-04-17 16:51:40 +0200

    Add navigation component

---
+++ src/components/navigation-components/Navigation.js
@@ -0,0 +1,14 @@
+/**
+ * Component for rendering the navigation bar.
+ *
+ * @returns {JSX.Element} The JSX element representing the Navigation component.
+ */
+const Navigation = () => {
+	return (
+		<div>
+			<p>Meny</p>
+		</div>
+	)
+}
+
+export default Navigation
