a44e48947a4a40f859f9294509a1b02e649f6b9b
Anja Willsund <aw22hs@student.lnu.se>
2024-04-23 14:28:07 +0200

    Add buttons to delete puzzle and to go back

--- src/components/puzzle-components/EditPuzzle.js
+++ src/components/puzzle-components/EditPuzzle.js
@@ -1,10 +1,32 @@
 
+import Button from '../Button'
+import { useContext } from 'react'
 import PuzzleForm from './PuzzleForm'
 import { useLocation } from 'react-router-dom'
+import { TokenContext } from '../contexts/TokenContext'
+import { useNavigate } from 'react-router-dom'
 
 const EditPuzzle = () => {
+  const navigate = useNavigate()
   const location = useLocation()
   const { puzzle } = location.state || {}
+  const fetchWithToken = useContext(TokenContext)
+
+  const handleClickDelete = async (id) => {
+    try {
+      const response = await fetchWithToken(`${process.env.REACT_APP_API_URL}/my/puzzles/${id}`, {
+        method: 'DELETE'
+      })
+      if (response.ok) {
+        console.log('Puzzle deleted')
+        navigate('/my-puzzles')
+      } else {
+        console.log('Could not delete puzzle')
+      }
+    } catch (error) {
+      console.log(error)
+    }
+  }
 
   return (
     <div>
@@ -13,6 +35,14 @@ const EditPuzzle = () => {
         method='PUT'
         navigateUrl={`/puzzles/${puzzle.id}`}
         buttonText='Spara'
+      />
+      <Button
+        buttonText='Radera pussel'
+        onClick={() => handleClickDelete(puzzle.id)}
+      />
+      <Button
+        buttonText='Tillbaka'
+        onClick={() => navigate(`/puzzles/${puzzle.id}`)} 
       />
     </div>
   )
