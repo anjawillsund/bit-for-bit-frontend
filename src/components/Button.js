9145856ffae981b7936f215efd80cc5bacd802f5
Anja Willsund <aw22hs@student.lnu.se>
2024-04-29 15:49:17 +0200

    Add image option for button

--- src/components/Button.js
+++ src/components/Button.js
@@ -6,12 +6,15 @@
  * @param {Function} props.onClick - The click event handler for the button.
  * @param {string} props.buttonText - The text content of the button.
  * @param {boolean} [props.disabled] - Whether the button is disabled.
+ * @param {string} [props.imageSrc] - The image source for the button.
+ * @param {string} [props.alt] - The alt text for the image.
  * @returns {JSX.Element} The JSX element representing the Button component.
  */
-const Button = ({ className, onClick, buttonText, disabled }) => {
+const Button = ({ className, onClick, buttonText, disabled, imageSrc, alt }) => {
 
   return (
     <button onClick={onClick} className={className} disabled={disabled}>
+      {imageSrc && <img src={imageSrc} alt={alt} style={{ marginRight: buttonText ? '10px' : '0' }} />}
       {buttonText}
     </button>
   )
