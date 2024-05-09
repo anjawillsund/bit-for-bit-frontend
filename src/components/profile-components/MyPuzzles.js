8c446f0809409fb5a89063e3f2810934bf4a20d3
Anja Willsund <aw22hs@student.lnu.se>
2024-05-09 19:42:09 +0200

    Add filters

--- src/components/profile-components/MyPuzzles.js
+++ src/components/profile-components/MyPuzzles.js
@@ -1,9 +1,10 @@
 import Button from '../Button'
-import React from 'react'
-import { useContext, useEffect } from 'react'
+import { useContext, useEffect, useState } from 'react'
 import { PuzzleContext } from '../contexts/PuzzleContext'
 import { useNavigate, Link } from 'react-router-dom'
 import nullPuzzleImage from '../../assets/images/null-puzzle.jpeg'
+
+import filter from '../../assets/icons/filter.png'
 
 /**
  * Renders a list of the user's puzzles.
@@ -15,6 +16,10 @@ const MyPuzzles = () => {
   const navigate = useNavigate()
 
   const { fetchUserPuzzles, puzzlesArray, isLoadingPuzzles, setIsLoadingPuzzles } = useContext(PuzzleContext)
+
+  const [hideLentOut, setHideLentOut] = useState(false)
+  const [selectedPieces, setSelectedPieces] = useState([])
+  const [showFilters, setShowFilters] = useState(false)
 
   useEffect(() => {
     async function loadData() {
@@ -29,19 +34,57 @@ const MyPuzzles = () => {
     navigate('/add-puzzle')
   }
 
+  const toggleLentOutFilter = () => {
+    setHideLentOut(!hideLentOut)
+  }
+
+  const togglePieceFilter = (pieceNumber) => {
+    if (selectedPieces.includes(pieceNumber)) {
+      setSelectedPieces(selectedPieces.filter((piece) => piece !== pieceNumber))
+    } else {
+      setSelectedPieces([...selectedPieces, pieceNumber])
+    }
+  }
+
+  const uniquePieceNumbers = [...new Set(puzzlesArray.map((puzzle) => puzzle.piecesNumber))].filter(Boolean)
+
+  const handleToggleFilter = () => {
+    setShowFilters(!showFilters)
+  }
+
   return (
     <div className='puzzles'>
       <div className='add-puzzle-button-container'>
         <Button
+          id='filter-button'
+          onClick={handleToggleFilter}
+          imageSrc={filter}
+        />
+        <Button
           onClick={handleAddPuzzle}
           buttonText='+'
         />
+        {showFilters && (
+        <div className='piece-filter'>
+        <Button onClick={toggleLentOutFilter} buttonText={hideLentOut ? 'Show Lent Out' : 'Hide Lent Out'} />
+        {uniquePieceNumbers.map((pieceNumber) => (
+          <label key={pieceNumber}>
+            <input
+              type='checkbox'
+              checked={selectedPieces.includes(pieceNumber)}
+              onChange={() => togglePieceFilter(pieceNumber)}
+            />
+            {pieceNumber}
+          </label>
+        ))}
+      </div>
+        )}
       </div>
       {isLoadingPuzzles ? (
         <p>Laddar...</p>
       ) : (
         <ul>
-          {puzzlesArray.length > 0 ? (puzzlesArray.map(puzzle => (
+          {puzzlesArray.length > 0 ? (puzzlesArray.map((puzzle) => (hideLentOut && puzzle.isLentOut) || (selectedPieces.length > 0 && !selectedPieces.includes(puzzle.piecesNumber)) ? null : (
             <li key={puzzle.id} className="puzzle-item">
               <Link to={`/puzzles/${puzzle.id}`}>
                 <div className={`image-container ${puzzle.isLentOut ? 'lent-out' : ''}`}>
