9ce53202f2868dfc331444a93e785dbc8b32a4a7
Anja Willsund <aw22hs@student.lnu.se>
2024-05-12 17:07:00 +0200

    Remove unnecessary variables and method to other modules

--- src/components/contexts/PuzzleContext.js
+++ src/components/contexts/PuzzleContext.js
@@ -1,5 +1,4 @@
 import { createContext, useState } from 'react'
-import { useTokenContext } from './TokenContext'
 
 /**
  * Context for managing movie-related data and functions.
@@ -14,52 +13,23 @@ export const PuzzleContext = createContext()
  * @returns {JSX.Element} The JSX element representing the PuzzleContextProvider component.
  */
 const PuzzleContextProvider = ({ children }) => {
-  const fetchWithToken = useTokenContext()
-
-  const [isUserActive, setIsUserActive] = useState(false)
-  const [puzzles, setPuzzles] = useState([])
   const [puzzlesArray, setPuzzlesArray] = useState([])
   const [isLoadingPuzzles, setIsLoadingPuzzles] = useState(true)
-
-  const fetchUserPuzzles = async () => {
-    try {
-      console.log('Fetching user puzzles')
-      const response = await fetchWithToken(`${process.env.REACT_APP_API_URL}/my/puzzles`, {
-        method: 'GET'
-      })
-      if (response.ok) {
-        const puzzles = await response.json()
-        console.log(puzzles)
-        setPuzzlesArray(puzzles)
-      } else {
-        console.log('Could not fetch user puzzles')
-      }
-    } catch (error) {
-      console.log(error)
-    }
-  }
 
   /**
    * Resets the state of the PuzzleContextProvider component.
    */
   const resetState = () => {
-    setPuzzles([])
     setPuzzlesArray([])
     setIsLoadingPuzzles(true)
-    setIsUserActive(false)
   }
 
   return <PuzzleContext.Provider
     value={{
-      fetchUserPuzzles,
       isLoadingPuzzles,
-      isUserActive,
-      puzzles,
       puzzlesArray,
       resetState,
       setIsLoadingPuzzles,
-      setIsUserActive,
-      setPuzzles,
       setPuzzlesArray
     }}>
     {children}
