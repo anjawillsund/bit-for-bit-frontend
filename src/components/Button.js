3532051caeab609c9f66b748f9e6e97a62f7fffe
Anja Willsund <aw22hs@student.lnu.se>
2024-05-18 17:03:08 +0200

    Add comments and restructure

--- src/components/Button.js
+++ src/components/Button.js
@@ -2,18 +2,23 @@
  * Button component.
  *
  * @param {Object} props - The component props.
+ * @param {string} props.id - The id for the button.
  * @param {string} props.className - The class name for the button.
  * @param {Function} props.onClick - The click event handler for the button.
  * @param {string} props.buttonText - The text content of the button.
- * @param {boolean} [props.disabled] - Whether the button is disabled.
  * @param {string} [props.imageSrc] - The image source for the button.
  * @param {string} [props.alt] - The alt text for the image.
  * @returns {JSX.Element} The JSX element representing the Button component.
  */
-const Button = ({ id, className, onClick, buttonText, disabled, imageSrc, alt }) => {
+const Button = ({ id, className, onClick, buttonText, imageSrc, alt }) => {
 
   return (
-    <button onClick={onClick} id={id} className={className} disabled={disabled}>
+    <button
+      onClick={onClick}
+      id={id}
+      className={className}
+    >
+      {/* If the button has an image source, display the image; otherwise, display the button text. */}
       {imageSrc && <img src={imageSrc} alt={alt} style={{ marginRight: buttonText ? '10px' : '0' }} />}
       {buttonText}
     </button>
