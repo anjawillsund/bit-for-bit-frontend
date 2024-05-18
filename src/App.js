133ac1a4e3ce9d31ac8c45d54aff8da50f4a91e9
Anja Willsund <aw22hs@student.lnu.se>
2024-05-18 17:08:06 +0200

    Minor restructure

--- src/App.js
+++ src/App.js
@@ -43,7 +43,7 @@ function App() {
                   </div>
                 </div>
               </>
-            : <Navigate to='/' />} />
+              : <Navigate to='/' />} />
           </Routes>
         </div>
       </PuzzleContextProvider>
